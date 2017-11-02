2eb09183a33b7b5a6bb1539c9823f6f0c01f1848
Jiajie Yao <jiajie.yao@intel.com>
2017-11-02 17:31:47 +0800

    Add model 4.1

--- scripts/predict4.py
+++ scripts/predict4.py
@@ -118,6 +118,48 @@ def predict_4(seg_net, image, thresh=0.5, display=False):
     cv2.imwrite('seg.png', image)
 
 
+def predict_4_1(seg_net, image, thresh=0.5, display=False):
+    for _ in range(10):
+        tic = time.time()
+        original_height, original_width, _ = image.shape
+        input_data = convert(image)
+        fitsize_img = fit_size([image, ])[0]
+        edge_feature = get_normed_edge_feature(fitsize_img)
+        edge_feature = cv2.resize(edge_feature, None, fx=1.0/8, fy=1.0/8)
+        seg_net.blobs['image'].data[...] = input_data
+        seg_net.blobs['edge_feature'].data[...] = edge_feature[np.newaxis, np.newaxis, :, :]
+        output = seg_net.forward()
+        toc = time.time()
+        print('seg time total = {:f}.'.format(toc - tic))
+
+    seg = output['seg_out'][0]
+    seg = np.squeeze(seg)
+    seg = cv2.resize(seg, None, fx=8, fy=8)
+
+    if display:
+        seg_heatmap = cv2.normalize(seg, None, alpha=0.0, beta=255.0, norm_type=cv2.NORM_MINMAX)
+        seg_heatmap = seg_heatmap.astype(np.uint8)
+        cv2.imshow('seg_heatmap', seg_heatmap)
+        cv2.waitKey()
+
+    seg[seg > thresh] = person_label
+    seg[seg != person_label] = 0
+    seg = seg.astype(np.uint8)
+
+    mask = to_original_scale(seg, (original_height, original_width))
+    mask_color = np.zeros((original_height, original_width, 3), np.uint8)
+    mask_color[mask == person_label] = [0, 255, 0]
+
+    if np.count_nonzero(mask) > 0:
+        image[mask == person_label] = cv2.addWeighted(image[mask == person_label], 0.5,
+                                                      mask_color[mask == person_label], 0.5, 0)
+
+    if display:
+        cv2.imshow('segmentation', image)
+        cv2.waitKey()
+    cv2.imwrite('seg.png', image)
+
+
 if __name__ == '__main__':
     device = sys.argv[1]
     seg_prototxt = sys.argv[2]
@@ -134,8 +176,6 @@ if __name__ == '__main__':
         
     seg_net = caffe.Net(seg_prototxt, seg_weights, caffe.TEST)
     img = cv2.imread(image_path)
-    predict_4(seg_net, img, thresh)
+    # predict_4(seg_net, img, thresh)
+    predict_4_1(seg_net, img, thresh)
 
-
-    
-        

6958d1aa3df28bd7f4e4f514541723e38f19871f
Jiajie Yao <jiajie.yao@intel.com>
2017-11-09 13:44:54 +0800

    minor changes.

--- train.sh
+++ train.sh
@@ -3,10 +3,12 @@
 caffe_root=/home/jiajie/openpose/3rdparty/caffe
 
 version=$1
+weights=$2
+gpu=$3
 
 cd models
-${caffe_root}/build/tools/caffe train -solver person_seg_net_${version}_solver.prototxt  -weights person_seg_net_${version}_init_weights.caffemodel  -gpu 1
-#${caffe_root}/build/tools/caffe train -solver person_seg_net_3.1_solver.prototxt  -snapshot backup/person_seg_net_3.1_iter_180765.solverstate  -gpu 0
+#${caffe_root}/build/tools/caffe train -solver person_seg_net_${version}_solver.prototxt  -snapshot ${weights}  -gpu ${gpu}
+${caffe_root}/build/tools/caffe train -solver person_seg_net_${version}_solver.prototxt  -weights ${weights}  -gpu ${gpu}
 cd -
 
 
